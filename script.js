f2466b81f1a91ddec98361d3061c9580c65351c7
sitarelli <149289661+sitarelli@users.noreply.github.com>
2026-02-03 21:39:53 +0100

    allungati a 18 char nome classifica

--- script.js
+++ script.js
@@ -898,7 +898,7 @@ function disegnaLista(data) {
 
 window.salvaPunteggio = async function() {
     const nome = playerNameInput.value.trim();
-    if (nome.length === 0 || nome.length > 16) { alert("Inserisci un nome valido (1-16 caratteri)"); return; }
+    if (nome.length === 0 || nome.length > 18) { alert("Inserisci un nome valido (1-16 caratteri)"); return; }
     const btn = document.getElementById('btn-save'); if(btn) { btn.disabled = true; btn.innerText = "Salvataggio..."; }
     
     const { error } = await dbClient.from('classifica').insert([{ nome: nome, punteggio: score }]);
@@ -1053,4 +1053,5 @@ window.addEventListener('load', () => {
     });
 
 });
+
 
